e258e69d103ee8b622689cd6ce088ad663b024bb
Mark Eibes <mark.eibes@gmail.com>
2022-07-03 20:07:24 +0200

    Update Concurrent.js

--- src/React/Basic/DOM/Concurrent.js
+++ src/React/Basic/DOM/Concurrent.js
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom";
-const createRoot = ReactDOM.createRoot || ReactDOM.unstable_createRoot;
+const createRoot_ = ReactDOM.createRoot || ReactDOM.unstable_createRoot;
 const createBlockingRoot =
   ReactDOM.createBlockingRoot || ReactDOM.unstable_createBlockingRoot;
 
 export function createRoot(element) {
-  return () => createRoot(element);
+  return () => createRoot_(element);
 }
 
 export function createBlockingRoot(element) {
